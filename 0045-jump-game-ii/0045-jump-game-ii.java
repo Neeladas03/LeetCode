3dcce43b97777ad6097f10f4be474c7c971f494d
Neeladhasan.m <118243703+Neeladas03@users.noreply.github.com>
2025-01-08 12:14:10 +0530

    Time: 118 ms (8.61%), Space: 45.8 MB (10.26%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,15 +1,30 @@
 class Solution {
     public int jump(int[] nums) {
-        int n = nums.length;
-        int jumps = 0, currentEnd = 0, farthest = 0;
+        int n=nums.length;
+        int dp[]=new int[n];
+        Arrays.fill(dp,-1);
+        return helper(0,dp,nums);
+    }
 
-        for (int i = 0; i < n - 1; i++) {
-            farthest = Math.max(farthest, i + nums[i]);
-            if (i == currentEnd) {
-                jumps++;
-                currentEnd = farthest;
+    private int helper(int ind,int dp[],int nums[]){
+        int n=nums.length;
+        if(ind>=n-1) return 0;
+        if(nums[ind]==0)return Integer.MAX_VALUE;
+        
+        if(dp[ind]!=-1)return dp[ind];
+        int min=Integer.MAX_VALUE;
+        int max=Math.min(ind+nums[ind],n-1);
+
+        for(int i=ind+1;i<=max;i++){
+            int ans=helper(i,dp,nums);
+            if(ans!=Integer.MAX_VALUE){
+                min=Math.min(min,ans+1);
+
             }
         }
-        return jumps;
+
+        dp[ind]=min;
+        return dp[ind];
+        
     }
 }
