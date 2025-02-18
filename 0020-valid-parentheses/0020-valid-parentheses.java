e018581ad96810cc61783099d73bf97a65a2ae1a
Neeladhasan.m <118243703+Neeladas03@users.noreply.github.com>
2025-02-18 10:03:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,21 +1,32 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character>stack=new Stack<>();
-        for(char c:s.toCharArray()){
-            if(c=='('){
-                stack.push(')');
+        Stack<Character>st=new Stack<>();
+        if(s.length()<=1)return false;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='('||ch=='{'||ch=='['){
+                st.push(ch);
             }
-            else if(c=='{'){
-                stack.push('}');
+            else{
+                // if(st.isEmpty())return false;
+
+            if(ch==')' &&  st.peek()=='('){
+                st.pop();
             }
-            else if(c=='['){
-                stack.push(']');
+            else if(ch=='}' && st.peek()=='{'){
+                st.pop();
             }
-            else if(stack.isEmpty()|| stack.pop()!=c){
+            else if(ch==']' &&  st.peek()=='['){
+                st.pop();
+            }
+
+            else{
                 return false;
-            
+            }
+
+            }
+
         }
+        return st.isEmpty();
     }
-    return stack.isEmpty();
-}
 }
