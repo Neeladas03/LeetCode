af9b74b800f2ad6f730079efa1bc1f703fa1ecd4
Neeladhasan.m <118243703+Neeladas03@users.noreply.github.com>
2025-03-05 11:57:03 +0530

    Time: 2 ms (70.06%), Space: 42.4 MB (60.95%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -8,34 +8,29 @@ class Pair{
         this.time=time;
     }
 }
-class Solution {
+ class Solution {
     public int orangesRotting(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-
         Queue<Pair>q=new LinkedList<>();
         int seen[][]=new int[n][m];
-        int count=0;
+        int delrow[]={-1,0,1,0};
+        int delcol[]={0,1,0,-1};
         int freshcount=0;
+        int time=0;
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    q.add(new Pair(i,j,0));
-                    seen[i][j]=2;
+                    q.offer(new Pair(i,j,0));
                 }
-                else{
-                    seen[i][j]=0;
+                else if(grid[i][j]==1){
+                    freshcount++;
                 }
-
-                if(grid[i][j]==1)freshcount++;
             }
         }
-
-        int delrow[]={-1,0,1,0};
-        int delcol[]={0,1,0,-1};
-        int time=0;
-
+        
+        int rottencount=0;
         while(!q.isEmpty()){
             int nrow=q.peek().row;
             int ncol=q.peek().col;
@@ -43,23 +38,19 @@ class Solution {
             q.poll();
 
             time=Math.max(time,ntime);
+
             for(int i=0;i<4;i++){
                 int r=nrow+delrow[i];
                 int c=ncol+delcol[i];
-
-                if(r>=0 &&r<n &&c>=0 && c<m && seen[r][c]==0 && grid[r][c]==1){
-                    q.add(new Pair(r,c,ntime+1));
+                if(r>=0 && r<n && c>=0 && c<m && seen[r][c]!=2 && grid[r][c]==1){
                     seen[r][c]=2;
-                    count++;
+                    q.add(new Pair(r,c,ntime+1));
+                    rottencount++;
                 }
             }
-
         }
-          
-          if(count!=freshcount)return -1;
 
-          return time;
-
-
+        if(rottencount!=freshcount)return -1;
+        return time;
     }
 }
