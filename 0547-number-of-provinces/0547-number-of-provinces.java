b46dca34bf54c254279a0e528f8adad344da81fc
Neeladhasan.m <118243703+Neeladas03@users.noreply.github.com>
2025-03-05 10:47:37 +0530

    Time: 3 ms (32.86%), Space: 47.4 MB (74.95%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,37 +1,37 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        ArrayList<ArrayList<Integer>>ds=new ArrayList<>();
-        for(int i=0;i<isConnected.length;i++){
-            ds.add(new ArrayList<>());
+        int n=isConnected.length;
+        int m=isConnected[0].length;
+        boolean seen[]=new boolean[n];
+        ArrayList<ArrayList<Integer>>gp=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            gp.add(new ArrayList<>());
         }
 
-        for(int i=0;i<isConnected.length;i++){
-            for(int j=0;j<isConnected.length;j++){
-                if(isConnected[i][j]==1 && i!=j){
-                    ds.get(i).add(j);
-                    ds.get(j).add(i);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(isConnected[i][j]==1){
+                    gp.get(i).add(j);
+                    gp.get(j).add(i);
                 }
             }
         }
 
-        boolean seen[]=new boolean[ds.size()+1];
         int count=0;
-        for(int i=0;i<ds.size();i++){
-            if(!seen[i]){
-                count++;
-                dfs(i,ds,seen);
-            }
+        for(int i=0;i<n;i++){
+           if(!seen[i]){
+            count++;
+            bfs(i,gp,seen);
+           }
         }
-
         return count;
-
     }
 
-    public static void dfs(int node, ArrayList<ArrayList<Integer>>ds,boolean seen[]){
+    public void bfs(int node,ArrayList<ArrayList<Integer>>gp,boolean seen[]){
         seen[node]=true;
-        for(int nh:ds.get(node)){
+        for(int nh:gp.get(node)){
             if(!seen[nh]){
-                dfs(nh,ds,seen);
+                bfs(nh,gp,seen);
             }
         }
     }
