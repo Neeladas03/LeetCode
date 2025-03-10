486888bf5a89208be94912f8262e3ccc4b4a8f1c
Neeladhasan.m <118243703+Neeladas03@users.noreply.github.com>
2025-03-10 11:54:01 +0530

    Time: 0 ms (100%), Space: 41.6 MB (6.05%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,29 +1,17 @@
 class Solution {
-    private int helper(int arr[]){
-           int n=arr.length;
-           int prev=arr[0];
-           int prev2=0;
-           for(int i=1;i<n;i++){
-            int take=arr[i];
-            if(i>1) take+=prev2;
-            int not=0+prev;
-            int curr=Math.max(take,not);
-            prev2=prev;
-            prev=curr;
-           }
-           return prev;
-    }
-
-
     public int rob(int[] nums) {
         int n=nums.length;
-        if(n==1) return nums[0];
-        int arr1[]=new int[n];
-        int arr2[]=new int[n];
-        for(int i=0;i<n;i++){
-            if(i!=0) arr1[i]=nums[i];
-            if(i!=n-1) arr2[i]=nums[i];
+        if(n==1)return nums[0];
+        return Math.max(helper(nums,0,n-2),helper(nums,1,n-1));
+    }
+
+    public int helper(int nums[],int start,int end){
+        int prev2=0,prev1=0,curr=0;
+        for(int i=start;i<=end;i++){
+            curr=Math.max(nums[i]+prev2,prev1);
+            prev2=prev1;
+            prev1=curr;
         }
-        return Math.max(helper(arr1),helper(arr2));
+        return prev1;
     }
 }
