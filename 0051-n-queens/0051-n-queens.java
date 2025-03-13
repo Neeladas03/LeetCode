1d9f4805f1d81b225b23b7a17411f88d8e05773e
Neeladhasan.m <118243703+Neeladas03@users.noreply.github.com>
2025-03-13 21:05:34 +0530

    Time: 2 ms (87.51%), Space: 45.2 MB (25.32%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,47 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        char grid[][]=new char[n][n];
+        List<List<String>>ans=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            Arrays.fill(grid[i],'.');
+        }
+
+        helper(0,ans,grid,n);
+        return ans;
+    }
+
+    public void helper(int row,List<List<String>>ans,char grid[][],int n){
+        if(row==n){
+            ArrayList<String>current=new ArrayList<>();
+            for(char[]chars:grid){
+                current.add(new String(chars));
+            }
+            ans.add(current);
+            return;
+        }
+
+        for(int col=0;col<n;col++){
+            if(isSafe(row,col,grid,n)){
+                grid[row][col]='Q';
+                helper(row+1,ans,grid,n);
+                grid[row][col]='.';
+            }
+        }       
+    }
+
+    public static boolean isSafe(int row,int col,char grid[][],int n){
+          for(int i=0;i<row;i++){
+            if(grid[i][col]=='Q')return false;
+          }
+
+          for(int i=row-1,j=col-1;i>=0 && j>=0 ;i--,j--){
+            if(grid[i][j]=='Q')return false;
+          }
+
+          for(int i=row-1,j=col+1;i>=0 &&j<n;i--,j++){
+            if(grid[i][j]=='Q')return false;
+          }
+
+          return true;
+    }
+}
