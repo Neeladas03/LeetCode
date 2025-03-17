877c89f65aac31ede066b0939c691805bc2941d2
Neeladhasan.m <118243703+Neeladas03@users.noreply.github.com>
2025-03-17 16:23:00 +0530

    Time: 1 ms (97.78%), Space: 45.4 MB (88.26%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,24 +1,21 @@
 class Solution {
-    public boolean canPlaceFlowers(int[] nums, int n) {
-        int count = 0;
-        
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                // Check if the previous and next flowerbeds are empty or if it's an edge case
-                boolean leftEmpty = (i == 0 || nums[i - 1] == 0);
-                boolean rightEmpty = (i == nums.length - 1 || nums[i + 1] == 0);
-                
-                if (leftEmpty && rightEmpty) {
-                    nums[i] = 1;  // Plant a flower
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count=0;
+     
+        for(int i=0;i<flowerbed.length;i++){
+            if(flowerbed[i]==0){
+                boolean left=(i==0 || flowerbed[i-1]==0);
+                boolean right=(i==flowerbed.length-1 || flowerbed[i+1]==0);
+
+                if(left && right){
+                    flowerbed[i]=1;
                     count++;
-                    
-                    if (count >= n) {
-                        return true;  // Early exit if we've planted enough flowers
-                    }
+
+                    if(count>=n)return true;
                 }
             }
         }
-        
-        return count >= n;
+
+        return count>=n;
     }
 }
