a1b4d7c2cbad5490eb8cb72fc92db5db43b2078c
Neeladhasan.m <118243703+Neeladas03@users.noreply.github.com>
2025-03-31 07:41:54 +0530

    Time: 162 ms (37.19%), Space: 175 MB (33.47%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,33 +1,27 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-
-       List<List<Integer>>list=new ArrayList<>();
-       for(int i=0;i<n;i++){
-        list.add(new ArrayList<>());
-       }
-
-       for(int mat[]:edges){
-        list.get(mat[0]).add(mat[1]);
-        list.get(mat[1]).add(mat[0]);
-       }
-
-        boolean visited[]=new boolean[n];
-        Queue<Integer>q=new LinkedList<>();
-        visited[source]=true;
-        q.add(source);
-
-        while(!q.isEmpty()){
-            int node=q.poll();
-            if(node==destination)return true;
-
-            for(int neighbour:list.get(node)){
-                if(!visited[neighbour]){
-                    q.add(neighbour);
-                    visited[neighbour]=true;
-                }
-            }
+        List<List<Integer>>adjlist=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adjlist.add(new ArrayList<>());
         }
 
+        for(int edge[]:edges){
+            adjlist.get(edge[0]).add(edge[1]);
+            adjlist.get(edge[1]).add(edge[0]);
+        }
+
+        boolean seen[]=new boolean[n];
+        return dfs(adjlist,source,destination,seen);
+    }
+
+    public boolean dfs(List<List<Integer>>adjlist,int source,int destination,boolean seen[]){
+        if(source==destination)return true;
+        seen[source]=true;
+        for(int nh:adjlist.get(source)){
+            if(!seen[nh]){
+                if(dfs(adjlist,nh,destination,seen))return true;
+            }
+        }
         return false;
     }
 }
