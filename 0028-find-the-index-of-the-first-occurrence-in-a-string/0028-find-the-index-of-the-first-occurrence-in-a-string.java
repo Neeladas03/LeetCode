73625cbb6eef543da802c0d017db4c9a0a78ebd4
Neeladhasan.m <118243703+Neeladas03@users.noreply.github.com>
2025-06-06 10:43:59 +0530

    Time: 0 ms (100%), Space: 42 MB (7.86%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,8 +1,37 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        for(int i = 0, j = needle.length(); j<=haystack.length(); i++,j++){
-            if(haystack.substring(i,j).equals(needle)){
-                return i;
+        
+        int d=256;
+        int p=101;
+        int h=1;
+        int m=haystack.length(),n=needle.length();
+        if(n>m)return -1;
+        for(int i=0;i<n-1;i++){
+            h=(h*d)%p;
+        }
+
+        int nhash=0;
+        int whash=0;
+        for(int i=0;i<n;i++){
+            nhash=(d*nhash+needle.charAt(i))%p;
+            whash=(d*whash+haystack.charAt(i))%p;
+        }
+
+        for(int i=0;i<=m-n;i++){
+            if(nhash==whash){
+                boolean match=true;
+                for(int j=0;j<n;j++){
+                    if(haystack.charAt(i+j)!=needle.charAt(j)){
+                        match=false;
+                        break;
+                    }
+                }
+                if(match)return i;
+            }
+
+            if(i<m-n){
+                whash=(d*(whash-haystack.charAt(i)*h)+(haystack.charAt(i+n)))%p;
+                if(whash<0)whash+=p;
             }
         }
         return -1;
