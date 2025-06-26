f0c3e23859707ab63863ff0ae41652210f43dd08
Neeladhasan.m <118243703+Neeladas03@users.noreply.github.com>
2025-06-26 07:31:39 +0530

    Time: 26 ms (69.17%), Space: 57.1 MB (24.97%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        mergesort(nums,0,nums.length-1);
+        mergeSort(nums,0,nums.length-1);
         return nums;
     }
 
-    public void mergesort(int nums[],int low,int high){
+    public void mergeSort(int nums[],int low,int high){
         if(low<high){
-            int mid=(low+high)/2;
-            mergesort(nums,low,mid);
-            mergesort(nums,mid+1,high);
+            int mid=low+(high-low)/2;
+            mergeSort(nums,low,mid);
+            mergeSort(nums,mid+1,high);
             merge(nums,low,mid,high);
         }
     }
@@ -16,18 +16,12 @@ class Solution {
     public void merge(int arr[],int low,int mid,int high){
         int temp[]=new int[high-low+1];
         int i=low,j=mid+1,k=0;
-        while(i<=mid  && j<=high){
-            if(arr[i]<=arr[j]){
-               temp[k++]=arr[i++];
-            }
-            else{
-                temp[k++]=arr[j++];
-            }
+        while(i<=mid && j<=high){
+            if(arr[i]<=arr[j])temp[k++]=arr[i++];
+            else temp[k++]=arr[j++];
         }
-
         while(i<=mid)temp[k++]=arr[i++];
         while(j<=high)temp[k++]=arr[j++];
-
         for(int t=0;t<temp.length;t++){
             arr[low+t]=temp[t];
         }
