e0116cdfcbcb448a127973407ea23a499aff67ef
Neeladhasan.m <118243703+Neeladas03@users.noreply.github.com>
2025-07-01 09:49:10 +0530

    Time: 2 ms (68.31%), Space: 42.1 MB (93.1%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,56 +1,43 @@
-class Pair{
-    int row;
-    int col;
-    int time;
-    Pair(int row,int col,int time){
-        this.row=row;
-        this.col=col;
-        this.time=time;
-    }
-}
- class Solution {
+class Solution {
     public int orangesRotting(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-        Queue<Pair>q=new LinkedList<>();
-        int seen[][]=new int[n][m];
+        Queue<int[]>q=new LinkedList<>();
+        int freshOrange=0;
         int delrow[]={-1,0,1,0};
         int delcol[]={0,1,0,-1};
-        int freshcount=0;
-        int time=0;
-
+        int seen[][]=grid;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    q.offer(new Pair(i,j,0));
+                    q.add(new int[]{i,j});
                 }
-                else if(grid[i][j]==1){
-                    freshcount++;
-                }
+                else if(grid[i][j]==1)freshOrange++;
             }
         }
-        
-        int rottencount=0;
+
+        if(freshOrange==0)return 0;
+        if(q.isEmpty())return -1;
+
+        int min=-1;
         while(!q.isEmpty()){
-            int nrow=q.peek().row;
-            int ncol=q.peek().col;
-            int ntime=q.peek().time;
-            q.poll();
-
-            time=Math.max(time,ntime);
-
-            for(int i=0;i<4;i++){
-                int r=nrow+delrow[i];
-                int c=ncol+delcol[i];
-                if(r>=0 && r<n && c>=0 && c<m && seen[r][c]!=2 && grid[r][c]==1){
-                    seen[r][c]=2;
-                    q.add(new Pair(r,c,ntime+1));
-                    rottencount++;
+            int size=q.size();
+            while(size-->0){
+                int curr[]=q.poll();
+                int row=curr[0];
+                int col=curr[1];
+                for(int i=0;i<4;i++){
+                    int nrow=row+delrow[i];
+                    int ncol=col+delcol[i];
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && seen[nrow][ncol]==1){
+                        seen[nrow][ncol]=2;
+                        freshOrange--;
+                        q.add(new int[]{nrow,ncol});
+                    }
                 }
             }
+            min++;
         }
-
-        if(rottencount!=freshcount)return -1;
-        return time;
+        return (freshOrange==0)?min:-1;
     }
 }
