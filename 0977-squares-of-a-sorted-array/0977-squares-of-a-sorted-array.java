77557fba6f06cf1e765066e1f677f1eacdda3fc6
Neeladhasan.m <118243703+Neeladas03@users.noreply.github.com>
2025-08-06 10:44:35 +0530

    Time: 1 ms (100%), Space: 47.3 MB (45.05%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,44 +1,18 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int ans[]=new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            nums[i]=nums[i]*nums[i];
-        }
-
-        mergeSort(nums,0,nums.length-1);
-        for(int i=0;i<nums.length;i++){
-            ans[i]=nums[i];
-        }
-
-        return ans;
-    }
-
-    public void mergeSort(int nums[],int low,int high){
-        if(low<high){
-            int mid=low+(high-low)/2;
-            mergeSort(nums,low,mid);
-            mergeSort(nums,mid+1,high);
-            merge(nums,low,mid,high);
-        }
-    }
-
-    public void merge(int nums[],int low,int mid,int high){
-        int temp[]=new int[high-low+1];
-        int i=low,j=mid+1,k=0;
-        while(i<=mid && j<=high){
-            if(nums[i]<=nums[j]){
-                temp[k++]=nums[i++];
+        int  n=nums.length;
+        int left=0,right=n-1;
+        int res[]=new int[n];
+        for(int i=n-1;i>=0;i--){
+            if(Math.abs(nums[right])>Math.abs(nums[left])){
+                res[i]=nums[right]*nums[right];
+                right--;
             }
             else{
-                temp[k++]=nums[j++];
+                res[i]=nums[left]*nums[left];
+                left++;
             }
         }
-
-        while(i<=mid)temp[k++]=nums[i++];
-        while(j<=high)temp[k++]=nums[j++];
-
-        for(int t=0;t<temp.length;t++){
-            nums[low+t]=temp[t];
-        }
+        return res;
     }
 }
