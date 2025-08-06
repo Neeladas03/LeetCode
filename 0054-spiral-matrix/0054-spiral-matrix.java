2a790965d46e91a1f4c72d9923cf3fd89ef9e3cb
Neeladhasan.m <118243703+Neeladas03@users.noreply.github.com>
2025-08-06 20:03:42 +0530

    Time: 0 ms (100%), Space: 41.7 MB (42.63%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,46 +1,38 @@
-
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> result = new ArrayList<>();
-        if (matrix.length == 0) return result;
+        List<Integer>res=new ArrayList<>();
+        if(matrix.length==0)return res;
 
-        int top = 0;
-        int bottom = matrix.length - 1;
-        int left = 0;
-        int right = matrix[0].length - 1;
+        int top=0;
+        int bottom=matrix.length-1;
+        int left=0;
+        int right=matrix[0].length-1;
 
-        while (top <= bottom && left <= right) {
-            // Traverse from left to right
-            for (int j = left; j <= right; j++) {
-                result.add(matrix[top][j]);
+        while(top<=bottom && left<=right){
+            for(int i=left;i<=right;i++){
+                res.add(matrix[top][i]);
             }
             top++;
 
-            // Traverse from top to bottom
-            for (int i = top; i <= bottom; i++) {
-                result.add(matrix[i][right]);
+            for(int i=top;i<=bottom;i++){
+                res.add(matrix[i][right]);
             }
             right--;
 
-            // Check if there are rows left
-            if (top <= bottom) {
-                // Traverse from right to left
-                for (int j = right; j >= left; j--) {
-                    result.add(matrix[bottom][j]);
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
+                    res.add(matrix[bottom][i]);
                 }
                 bottom--;
             }
 
-            // Check if there are columns left
-            if (left <= right) {
-                // Traverse from bottom to top
-                for (int i = bottom; i >= top; i--) {
-                    result.add(matrix[i][left]);
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                    res.add(matrix[i][left]);
                 }
                 left++;
             }
         }
-
-        return result;
+        return res;
     }
 }
