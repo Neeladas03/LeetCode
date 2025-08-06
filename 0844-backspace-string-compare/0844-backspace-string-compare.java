e154a9518c02695fccee734d106002e184b893e5
Neeladhasan.m <118243703+Neeladas03@users.noreply.github.com>
2025-08-06 10:50:10 +0530

    Time: 1 ms (79.71%), Space: 41.6 MB (82.2%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,15 +1,18 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        return buildString(s).equals(buildString(t));
+        return helper(s).equals(helper(t));
     }
-    
-    private String buildString(String str) {
-        StringBuilder sb = new StringBuilder();
-        for (char c : str.toCharArray()) {
-            if (c != '#') {
-                sb.append(c);
-            } else if (sb.length() > 0) {
-                sb.deleteCharAt(sb.length() - 1);
+
+    public static String helper(String text){
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<text.length();i++){
+            if(text.charAt(i)!='#'){
+                sb.append(text.charAt(i));
+            }
+            else{
+                if(sb.length()>0){
+                sb.deleteCharAt(sb.length()-1);
+                }
             }
         }
         return sb.toString();
