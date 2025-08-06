271ef9dcf1f9ebaf49f930ed11edcf2d75219131
Neeladhasan.m <118243703+Neeladas03@users.noreply.github.com>
2025-08-06 10:10:16 +0530

    Time: 43 ms (5.08%), Space: 56.1 MB (99.7%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,10 +1,42 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer>seen=new HashSet<>();
-        for(int num:nums){
-            if(seen.contains(num))return true;
-            seen.add(num);
+        int n=nums.length;
+        mergeSort(nums,0,n-1);
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
         }
         return false;
+    }
+
+    public void mergeSort(int nums[],int low,int high){
+        if(low<high){
+            int mid=low+(high-low)/2;
+            mergeSort(nums,low,mid);
+            mergeSort(nums,mid+1,high);
+            merge(nums,low,mid,high);
+        }
+    }
+
+    public void merge(int nums[],int low,int mid,int high){
+        int temp[]=new int[high-low+1];
+        int i=low,j=mid+1,k=0;
+
+        while(i<=mid && j<=high){
+            if(nums[i]<=nums[j]){
+                temp[k++]=nums[i++];
+            }
+            else{
+                temp[k++]=nums[j++];
+            }
+        }
+
+        while(i<=mid)temp[k++]=nums[i++];
+        while(j<=high)temp[k++]=nums[j++];
+
+        for(int t=0;t<temp.length;t++){
+            nums[low+t]=temp[t];
+        }
     }
 }
